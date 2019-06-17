5617502a734dceda8e66fbe0996a84c7356088f9
Hasan-75 <m.hasan116114@gmail.com>
2019-06-17 12:22:31 +0600

    Created Text message model with customized admin

--- chatapp/admin.py
+++ chatapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from chatapp.models import TextMessage
+
+class TextMessageAdmin(admin.ModelAdmin):
+    list_display = ['__str__','sent_at']
+
+admin.site.register(TextMessage, TextMessageAdmin)
--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.utils.html import format_html
+
+
+class TextMessage(models.Model):
+     receiver = models.ForeignKey(User, related_name='Receiver', on_delete=models.CASCADE)
+     sender = models.ForeignKey(User, related_name='Sender', on_delete=models.CASCADE)
+     message = models.TextField()
+     sent_at = models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+         return format_html("<b>From:</b>"+self.sender.username+"<br><b>To:</b>"+self.receiver.username)
